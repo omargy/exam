9bf4099b703d7dbdaab537ad0760f111ddc1bda0
omargy <48441129+omargy@users.noreply.github.com>
2019-08-11 22:24:27 +0000

    Add files via upload

--- animatedmen.js
+++ animatedmen.js
@@ -1,38 +1,22 @@
-
 toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
 toggle_open_text = "Click";
 toggle_close_text = "Close";
 
 toggle.addEventListener(
-    "click",
-    function () {
-        nav.classList.toggle("open");
+	"click",
+	function() {
+		nav.classList.toggle("open");
 
-        if (nav.classList.contains("open")) {
-            toggle.innerHTML = toggle_close_text;
-        } else {
-            toggle.innerHTML = toggle_open_text;
-        }
-    },
-    false
+		if (nav.classList.contains("open")) {
+			toggle.innerHTML = toggle_close_text;
+		} else {
+			toggle.innerHTML = toggle_open_text;
+		}
+	},
+	false
 );
 
-setTimeout(function () {
-    nav.classList.toggle("open");
+setTimeout(function() {
+	nav.classList.toggle("open");
 }, 800);
-    
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-for(i = 0; i < acc.length; i++){
-    acc[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block"){
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-        });
-    }
